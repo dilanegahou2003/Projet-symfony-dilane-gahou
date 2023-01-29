ea2f9b7ff9bd3e16a66256ebca8c2fe14e759e9d
dilanegahou2003 <120408803+dilanegahou2003@users.noreply.github.com>
2023-01-29 21:32:53 +0100

    Add files via upload

---
+++ proejt dilane symfony.php
@@ -0,0 +1,13 @@
+<form action="login.php" method="post">
+  <div>
+    <label for="username">Nom d'utilisateur :</label>
+    <input type="text" id="username" name="username">
+  </div>
+  <div>
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password">
+  </div>
+  <div>
+    <input type="submit" value="Se connecter">
+  </div>
+</form>
